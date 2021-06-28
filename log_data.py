9d2f067c943d9be904e4ba83aa5a806a719d8d1c
luisjwhitaker <luisjwhitaker@gmail.com>
2021-06-28 23:08:25 +0800

    adding sqlite3 support for saving time-activity logs.

--- log_data.py
+++ log_data.py
@@ -1,6 +1,27 @@
-def log_sqlite(*data, database_name=None):
-    for item in data:
-        print(item)
+import sqlite3
+#def log_sqlite(*data, database_name=None):
+#    print(data)
+
+    # CREATE sqlite3 database:
+    """conn = sqlite3.connect('time.db')
+    c = conn.cursor()
+    c.execute('''CREATE Table logs (
+            time_elapsed text,
+            task_selected text
+            )''')
+    conn.commit()
+    conn.close()"""
+
+    # INSERT INTO sqlite3 database:
+    """conn = sqlite3.connect('stool_log.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO logs VALUES(:time_elapsed,:task_selected)",{
+            'time_elapsed': dt.strftime("%B %d,%Y"),
+            'task_selected': dt.strftime("%H:%M %Z")
+    })
+    conn.commit()
+    conn.close()
+    messagebox.showinfo('showinfo','submitted')"""
 
 '''
     This is meant to be an intermediery between the data I track and what database it is stored.
